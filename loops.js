dad863b789ec91a31b0956e85d43d01e013641cc
tpeck428 <toripeck73@gmail.com>
2024-02-28 13:16:22 -0500

    testing code, no significant changes

--- loops.js
+++ loops.js
@@ -43,7 +43,7 @@
 // Create a loop that searches for the next prime number, starting at n and incrementing from there.
 //As soon as you find the prime number, log that number and exit the loop.
 
-// let num = 12;
+// let num = 1355;
 // let count = 0;
 // let foundPrime = false;
 // while (!foundPrime) {
@@ -64,9 +64,9 @@
 
 //Part 3: Feeling Loopy
 //First Example
-// let data = "ID, Name, Occupation, Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
-// let rows = data.split("\n");
-// console.log(rows);
+let data = "ID, Name, Occupation, Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+let rows = data.split("\n");
+console.log(rows);
 
 //Second Example
 // let data = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)/n0.00,0.050,0.050/n0.49,0.066,0.066/n0.98,0.087,0.080/n1.47,0.116,0.108/n1.96,0.142,0.138/n2.45,0.166,0.158/n2.94,0.193,0.174/n3.43,0.204,0.192/n3.92,0.226,0.205/n4.41,0.238,0.232"
